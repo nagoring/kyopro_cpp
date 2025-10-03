9a33c9d85265e1f81235987f39307c8127040e19
nagomi <nagomi.github@gmail.com>
2025-10-03 17:53:35 +0900

    update

---
+++ A/abc400_a.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    const int MAX_T = 400;
+    int A = 0;
+    int B = 0;
+    cin >> A;
+    B = MAX_T / A;
+
+    if (MAX_T%A == 0) {
+        std::cout << B << std::endl;
+    }else {
+        std::cout << -1 << std::endl;
+    }
+    return 0;
+}
