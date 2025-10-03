8cd7142a18730588bd8858211f1fd48263e56573
nagomi <nagomi.github@gmail.com>
2025-10-03 23:18:11 +0900

    abc212

---
+++ A/abc212.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <ostream>
+
+int main(int argc, char *argv[]) {
+	int a = 0;
+	int b = 0;
+	std::cin >> a >> b;
+
+	if (0 < a && b == 0) {
+		std::cout << "Gold" << std::endl;
+		return 0;
+	}
+	if (0 == a && 0 < b) {
+		std::cout << "Silver" << std::endl;
+		return 0;
+	}
+	if (0 < a && 0 < b) {
+		std::cout << "Alloy" << std::endl;
+		return 0;
+	}
+
+	std::cout << b << std::endl;
+	return 0;
+}
