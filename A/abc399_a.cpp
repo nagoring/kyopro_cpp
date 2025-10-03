9817c50aec20e0be83b7285254f8d64eb0e20f24
nagomi <nagomi.github@gmail.com>
2025-10-03 20:12:22 +0900

    399_a

---
+++ A/abc399_a.cpp
@@ -0,0 +1,23 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+#include <ranges>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int N = 0;
+    string S = "";
+    string D = "";
+    cin >> N;
+    cin >> S;
+    cin >> D;
+
+
+    int cnt = static_cast<int>(ranges::count_if(views::iota(0, (int)S.size()), [&](int i){ return S[i] != D[i]; }));
+
+    cout << cnt << endl;
+
+    return 0;
+}
---
+++ version.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+int main() {
+    std::cout << "__cplusplus = " << __cplusplus << std::endl;
+
+#ifdef __GNUC__
+    std::cout << "GCC version: " << __GNUC__ << "." << __GNUC_MINOR__
+              << "." << __GNUC_PATCHLEVEL__ << std::endl;
+#endif
+
+#ifdef __clang__
+    std::cout << "Clang version: " << __clang_major__ << "." << __clang_minor__
+              << "." << __clang_patchlevel__ << std::endl;
+#endif
+
+#ifdef _MSC_VER
+    std::cout << "MSVC version: " << _MSC_VER << std::endl;
+#endif
+
+    return 0;
+}
