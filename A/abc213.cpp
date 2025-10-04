fe3a296f2a1c61b8c8bae581ea6ce7b4824c1e8f
nagomi <nagomi.github@gmail.com>
2025-10-04 15:55:01 +0900

    abc213cpp

---
+++ A/abc213.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int a, b;
+	cin >> a >> b;
+
+
+	cout << (a ^ b) << endl;
+
+	return 0;
+
+}
