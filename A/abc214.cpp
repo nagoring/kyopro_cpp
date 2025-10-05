50f6cc85ba2886ff17bff580d9e631ab469822f5
nagomi <nagomi.github@gmail.com>
2025-10-05 16:25:15 +0900

    abc214

---
+++ A/abc214.cpp
@@ -0,0 +1,23 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <ostream>
+
+int main(int argc, char *argv[]) {
+	int n = 0;
+	std::cin >> n;
+
+	if (std::clamp(n,1,125) == n) {
+		std::cout << "4" << std::endl;
+		return 0;
+	}
+	if (std::clamp(n,126,211) == n) {
+		std::cout << "6" << std::endl;
+		return 0;
+	}
+	if (std::clamp(n,212,214) == n) {
+		std::cout << "8" << std::endl;
+		return 0;
+	}
+
+	return 0;
+}
