a16a3b1dcebfade09d3eac0ffd7a54349ba29c87
nagomi <nagomi.github@gmail.com>
2025-10-07 15:06:03 +0900

    abc215_a

---
+++ A/abc215_a.cpp
@@ -0,0 +1,22 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <ostream>
+
+int main(int argc, char *argv[]) {
+	std::string line;
+	// std::cin >> line;
+	std::getline(std::cin, line);
+
+	std::erase(line, '\r');
+	std::erase(line, '\n');
+
+	if (line == "Hello,World!") {
+		std::cout << "AC" << std::endl;
+		return 0;
+	}
+	std::cout << "WA" << std::endl;
+
+
+	return 0;
+}
+
