8941e38f418b38118019d9b92d61ce087c0323d4
nagomi <nagomi.github@gmail.com>
2025-10-19 18:52:59 +0900

    abc217_a

---
+++ A/abc217_a.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <ostream>
+
+int main(int argc, char *argv[]) {
+
+	std::string line;
+	std::getline(std::cin, line);
+	size_t pos = line.find(' ');
+	std::string S = line.substr(0,pos);
+	std::string T = line.substr(pos + 1);
+
+
+	if (S < T) {
+		std::cout << "Yes" << std::endl;
+		return 0;
+	}
+	if (S > T) {
+		std::cout << "No" << std::endl;
+		return 0;
+	}
+
+	return 0;
+}
+
